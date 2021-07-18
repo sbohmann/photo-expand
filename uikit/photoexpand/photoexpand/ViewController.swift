36963dbc253be84d8a05a5a43a39d66a5f6e6bf5
sbohmann <mail@sebastian-bohmann.com>
2021-07-18 14:20:48 +0200

    Improved the localization example

--- uikit/photoexpand/photoexpand/ViewController.swift
+++ uikit/photoexpand/photoexpand/ViewController.swift
@@ -2,13 +2,16 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBAction func valueChanged(_ sender: UISlider) {
+        print(sender.value)
         let format = NSLocalizedString("years_on_hills", comment: "a pluralization example")
         print(format)
-        print(String.init(format: format, 0, 5))
-        print(String.init(format: format, 1, 4))
-        print(String.init(format: format, 2, 1))
-        print(String.init(format: format, 3, 2))
-        print(String.init(format: format, 4, 3))
-        print(String.init(format: format, 5, 0))
+//        print(String.init(format: format, 0, 5))
+//        print(String.init(format: format, 1, 4))
+//        print(String.init(format: format, 2, 1))
+//        print(String.init(format: format, 3, 2))
+//        print(String.init(format: format, 4, 3))
+//        print(String.init(format: format, 5, 0))
+        print(String.init(format: format, 3 * Int(sender.value.rounded()), Int(sender.value.rounded())))
+        print(String.init(format: format, Int(sender.value.rounded()), 3 * Int(sender.value.rounded())))
     }
 }
